ceb5328bd8a870a6b1a7e21bf46fa45bdb23376a
Batu <batublob@gmail.com>
2024-06-19 19:23:52 +0100

    use the proper ohur

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { $fetch } from "ofetch";
 import qs from "qs";
 import { ListensPerDay, Stats, TimeOfDay } from "./types";
@@ -14,11 +15,12 @@ class API {
     before?: Date,
     after?: Date,
   ): Promise<ListensPerDay[]> => {
+    const utcHour = dayjs.utc().hour();
     const queryString = qs.stringify({
       before: before?.valueOf(),
       after: after?.valueOf(),
       userId,
-      hour: 1,
+      hour: utcHour,
     });
 
     return $fetch(API.makeUrl(`/aggregate/listensPerDay?${queryString}`));
@@ -29,20 +31,22 @@ class API {
     before?: Date,
     after?: Date,
   ): Promise<TimeOfDay[]> => {
+    const utcHour = dayjs.utc().hour();
     const queryString = qs.stringify({
       before: before?.valueOf(),
       after: after?.valueOf(),
       userId,
-      hour: 1,
+      hour: utcHour,
     });
 
     return $fetch(API.makeUrl(`/aggregate/timeOfDay?${queryString}`));
   };
 
   static stats = async (userId: string): Promise<Stats> => {
+    const utcHour = dayjs.utc().hour();
     const queryString = qs.stringify({
       userId,
-      hour: 1,
+      hour: utcHour,
     });
 
     return $fetch(API.makeUrl(`/aggregate/stats?${queryString}`));
